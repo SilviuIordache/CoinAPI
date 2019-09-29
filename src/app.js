c1ad86ec4421ad8b310f9a24fef7c7ee7c8c2466
Silviu <silviu.iordache92@gmail.com>
2019-09-29 22:09:55 +0300

    Implement basic get request.

--- src/app.js
+++ src/app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const fs = require('fs');
 
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res
-    .status(200)
-    .json({ msg: 'Hello World!' });
+const expenses = JSON.parse(
+  fs.readFileSync(`${__dirname}/../data/expenses.json`),
+);
+
+app.get('/expenses', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: expenses.length,
+    data: expenses,
+  });
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}!`);
+});
